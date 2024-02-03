641972fc45f6c934a7e2d20e653c811fec0eb92c
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-02-03 13:38:52 -0500

    log with user

--- src/remix/root.tsx
+++ src/remix/root.tsx
@@ -50,14 +50,14 @@ export const meta: MetaFunction = () => {
 }
 
 export async function loader(args: LoaderFunctionArgs) {
+  const user = await authenticator.isAuthenticated(args.request)
   logger.debug(
     {
       URL: args.request.url,
+      userID: user?.id,
     },
     "loaded URL"
   )
-
-  const user = await authenticator.isAuthenticated(args.request)
 
   // Env vars for frontend
   const ENV: { [key: string]: string } = {}
