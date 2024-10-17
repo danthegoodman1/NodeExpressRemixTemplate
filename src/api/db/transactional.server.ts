ac7b8d411e3c1cecee20fb627b4aae6c53630e4e
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-10-16 17:27:19 -0700

    new transactional

--- src/api/db/transactional.server.ts
+++ src/api/db/transactional.server.ts
@@ -1,16 +1,29 @@
 import { Database } from "sqlite"
 import { pool } from "./db.server.js"
 
-export default async function inTransaction<T>(
-  fn: (conn: Database) => Promise<T>
+export async function inTransaction(
+  fn: (conn: Database) => Promise<any>
 ): Promise<void> {
   const conn = await pool.acquire()
   try {
-    await conn.run("BEGIN IMMEDIATE")
+    await conn.exec("BEGIN IMMEDIATE")
     await fn(conn)
-    await conn.run("COMMIT")
+    await conn.exec("COMMIT")
   } catch (e) {
-    await conn.run("ROLLBACK")
+    await conn.exec("ROLLBACK")
+    throw e
+  } finally {
+    await pool.release(conn)
+  }
+}
+
+export async function withConn(
+  fn: (conn: Database) => Promise<any>
+): Promise<void> {
+  const conn = await pool.acquire()
+  try {
+    await fn(conn)
+  } catch (e) {
     throw e
   } finally {
     await pool.release(conn)
